e9e44a67799691b94d095c507fe9f2363bc2c249
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-08-26 00:01:29 +0500

    creating functions in a file processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,1 +1,22 @@
-def pass
+from widget import get_date
+
+def filter_by_state(list_of_data: list[dict], state='EXECUTED') -> list[dict]:
+    '''Функция принимает список словарей и возвращает новый список со словарями
+     у которых значение по ключу "state" равно "EXECUTED"'''
+
+    result = []
+    for i in list_of_data:
+        if i['state'] == state:
+            result.append(i)
+    return result
+
+
+def sort_by_date(list_of_data: list[dict], sort_order=False) -> list[dict]:
+    '''
+    Возвращает список словарей, отсортированный по ключу "date"
+    :param list_of_data: исходный список словарей
+    :param sort_order: порядок сортировки (опционально)
+    :return: отсортированный список словарей
+    '''
+
+    return sorted(list_of_data, key=lambda list_of_data: get_date(list_of_data['date']), reverse=sort_order)
