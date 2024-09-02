93191b9dd2d329acae6077cf964a02da3b2f0c6d
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-09-03 00:04:28 +0500

    corrected masks.py

--- src/masks.py
+++ src/masks.py
@@ -1,12 +1,15 @@
 def get_mask_card_number(number_card: str) -> str:
     """принимает номер карты и возвращает ее маску, вне зависимости от длины номера карты"""
 
+    if not number_card.isdigit():
+        return 'некорректные данные'
+
     number_card_list = list(number_card)  # перевод номера карты из типа строка в тип список
 
     for i in range(6, len(number_card_list) - 4):  # замена части цифр на *
         number_card_list[i] = "*"
 
-    for i in range(4, len(number_card_list), 5):  # разделение номера карты на блоки по 4 цифры
+    for i in range(4, int(len(number_card_list) * 1.2), 5):  # разделение номера карты на блоки по 4 цифры
         number_card_list[i:i] = " "
 
     return "".join(number_card_list)
---
+++ tests/conftest.py
@@ -0,0 +1,9 @@
+# import pytest
+#
+# @pytest.fixture
+# def fixture_number_card():
+#     return '7000792289606361'
+#
+#
+# @pytest.mark.parametrize(number,
+#                          ['7000792289606361', '7000792289606361883402', 'number_card', ''])
