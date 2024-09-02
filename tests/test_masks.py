b3a96aefacc0794615a9138d1898ffbe06cf00ba
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-09-03 00:03:35 +0500

    add test_masks.py

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,10 +1,12 @@
-import unittest
+import pytest
 
+from src.masks import get_mask_card_number, get_mask_account
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
-
-
-if __name__ == '__main__':
-    unittest.main()
+@pytest.mark.parametrize('number_card, mask_card',
+                         [('7000792289606361', '7000 79** **** 6361'),
+                          ('7000792289606361883402', '7000 79** **** **** **34 02'),
+                          ('number_card', 'некорректные данные'),
+                          ('0000', '0000'),
+                          ('', 'некорректные данные')])
+def test_get_mask_card_number(number_card, mask_card):
+    assert get_mask_card_number(number_card) == mask_card
