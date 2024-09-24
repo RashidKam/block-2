dedcd0ed2c7fa2dccdebc93d0adcfccfce336d52
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-09-24 14:21:04 +0500

    corrected file test_processing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,27 +1,31 @@
 import pytest
 
 
-@pytest.fixture
-def data_for_verification_processing():
-    return [
-        (
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-                {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-                {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-            ],
-            None,
-            [
-                {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-                {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-            ],
-        ),
-        ([], None, []),
-    ]
+# @pytest.fixture
+# def data_for_verification_processing():
+#     return [
+#         (
+#             [
+#                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#                 {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+#                 {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+#             ],
+#             None,
+#             [
+#                 {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+#                 {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+#             ],
+#         ),
+#         ([], None, []),
+#     ]
 
 
-#
-#
-# @pytest.mark.parametrize(number,
-#                          ['7000792289606361', '7000792289606361883402', 'number_card', ''])
+# @pytest.fixture
+# def data_for_test_sort_by_date_value_error():
+#     return [
+#         {"id": 41428829, "state": "EXECUTED", "date": "19-07-03T18:35:29.512364"},
+#         {"id": 939719570, "state": "EXECUTED", "date": "18-06-30T02:08:58.425572"},
+#         {"id": 594226727, "state": "CANCELED", "date": "18-09-12T21:27:25.241689"},
+#         {"id": 615064591, "state": "CANCELED", "date": "18-10-14T08:21:33.419441"},
+#     ]
