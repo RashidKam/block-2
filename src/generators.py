b4397bffefe5a7c6558a4f8e37f3fa5919dd84d8
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-09-25 23:57:20 +0500

    corrected src/generators.py, tests/test_generators.py, tests/test_masks.py

--- src/generators.py
+++ src/generators.py
@@ -1,26 +1,32 @@
 from src.masks import card_number
 
+
 def filter_by_currency(list_of_transactions: list[dict], currency: str = None):
+    """
+    функци принимает на вход список транзакции и возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    :param list_of_transactions: список транзакций в виде списка словарей
+    :param currency: заданная валюта транзакции
+    :return: итератор транзакция заданной валюты
+    """
     for transaction in list_of_transactions:
-        if currency.lower() == transaction["operationAmount"]["currency"]["name"].lower():
+        if currency.lower() == transaction["operationAmount"]["currency"]["code"].lower():
             yield transaction
 
 
 def transaction_descriptions(list_of_transactions: list[dict]):
+    '''
+    Функция поочередно возвращает описание каждой операции
+    :param list_of_transactions: Транзакции в виде списка словарей
+    :return: итератор содержащий описание всех операций
+    '''
     return (x["description"] for x in list_of_transactions)
 
 
 def card_number_generator():
-    for i in range(1, 10 ** 16):
-        if len(str(i)) < 16:
-            i = '0' * (16 - len(str(i))) + str(i)
-            yield card_number(str(i))
-        yield card_number(str(i))
-
-print(next(card_number_generator()))
-print(next(card_number_generator()))
-print(next(card_number_generator()))
-print(next(card_number_generator()))
-print(next(card_number_generator()))
-print(next(card_number_generator()))
-print(next(card_number_generator()))
+    '''
+    Генератор номеров карт начиная с '0000 0000 0000 0001' и заканчивая '9999 9999 9999 9999'
+    :return: номер карты в виде 1234 1234 1324 1234
+    '''
+    for i in range(1, 10**16):
+        result = card_number(str(i))
+        yield result
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,6 +1,6 @@
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions
+from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
 
 
 def test_filter_by_currency(data_of_transactions):
@@ -16,3 +16,11 @@ def test_transaction_descriptions(data_of_transactions):
         "Перевод со счета на счет",
         "Перевод с карты на карту",
     ]
+
+
+def test_card_number_generator():
+    generator_ = card_number_generator()
+    assert next(generator_) == "0000 0000 0000 0001"
+    assert next(generator_) == "0000 0000 0000 0002"
+    assert next(generator_) == "0000 0000 0000 0003"
+    assert next(generator_) == "0000 0000 0000 0004"
