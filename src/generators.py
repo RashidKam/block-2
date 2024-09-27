927fd332b68c3f8c1a71a9785dad085c9ba3e477
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-09-27 23:23:21 +0500

    corrected generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,32 +1,35 @@
 from src.masks import card_number
 
 
-def filter_by_currency(list_of_transactions: list[dict], currency: str = None):
+def filter_by_currency(list_of_transactions: list[dict], currency: str = None) -> GeneratorExit:
     """
-    функци принимает на вход список транзакции и возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной
+    функци принимает на вход список транзакции и возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной.
+    Если валюта не заданаб вернёт весь список транзакций.
     :param list_of_transactions: список транзакций в виде списка словарей
     :param currency: заданная валюта транзакции
     :return: итератор транзакция заданной валюты
     """
     for transaction in list_of_transactions:
-        if currency.lower() == transaction["operationAmount"]["currency"]["code"].lower():
+        if currency == None:
+            yield transaction
+        elif currency.lower() == transaction["operationAmount"]["currency"]["code"].lower():
             yield transaction
 
 
-def transaction_descriptions(list_of_transactions: list[dict]):
-    '''
+def transaction_descriptions(list_of_transactions: list[dict]) -> GeneratorExit:
+    """
     Функция поочередно возвращает описание каждой операции
     :param list_of_transactions: Транзакции в виде списка словарей
     :return: итератор содержащий описание всех операций
-    '''
+    """
     return (x["description"] for x in list_of_transactions)
 
 
-def card_number_generator():
-    '''
+def card_number_generator() -> GeneratorExit:
+    """
     Генератор номеров карт начиная с '0000 0000 0000 0001' и заканчивая '9999 9999 9999 9999'
     :return: номер карты в виде 1234 1234 1324 1234
-    '''
+    """
     for i in range(1, 10**16):
         result = card_number(str(i))
         yield result
