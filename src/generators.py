37c0ade0b241a3d152a4969d01677e623a13e67e
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-09-29 22:24:36 +0500

    corrected generators.py

--- src/generators.py
+++ src/generators.py
@@ -1,14 +1,14 @@
 from src.masks import card_number
-from typing import Iterable
+from typing import Iterable, Any
 
 
-def filter_by_currency(list_of_transactions: list[dict], currency: str = None) -> GeneratorExit:
+def filter_by_currency(list_of_transactions: list[dict], currency: str = None) -> Iterable[Any]:
     """
     функци принимает на вход список транзакции и возвращает итератор, который поочередно выдает транзакции, где валюта операции соответствует заданной.
     Если валюта не заданаб вернёт весь список транзакций.
     :param list_of_transactions: список транзакций в виде списка словарей
     :param currency: заданная валюта транзакции
-    :return: итератор транзакция заданной валюты
+    :return: итератор транзакция заданной валютыcle
     """
     for transaction in list_of_transactions:
         if currency == None:
@@ -17,7 +17,7 @@ def filter_by_currency(list_of_transactions: list[dict], currency: str = None) -
             yield transaction
 
 
-def transaction_descriptions(list_of_transactions: list[dict]) -> GeneratorExit:
+def transaction_descriptions(list_of_transactions: list[dict]) -> Iterable[Any]:
     """
     Функция поочередно возвращает описание каждой операции
     :param list_of_transactions: Транзакции в виде списка словарей
@@ -26,7 +26,7 @@ def transaction_descriptions(list_of_transactions: list[dict]) -> GeneratorExit:
     return (x["description"] for x in list_of_transactions)
 
 
-def card_number_generator() -> GeneratorExit:
+def card_number_generator() -> Iterable[Any]:
     """
     Генератор номеров карт начиная с '0000 0000 0000 0001' и заканчивая '9999 9999 9999 9999'
     :return: номер карты в виде 1234 1234 1324 1234
