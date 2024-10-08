10708f5cbf847cacf0b9d1e090411dd553e8bc4d
Kamalutdinov_Rashid <kamalutdinov.r.r@yandex.ru>
2024-10-08 21:15:54 +0500

    add test_decorators.py and corected decorators.py

--- src/decorators.py
+++ src/decorators.py
@@ -2,37 +2,49 @@ from time import time
 
 
 def log(filename: str = ""):
+    '''
+    Данный декоратор записывает в файл/выводит в консоль логи работы функции
+    :param filename: наименование файла, в который будут записываться логи работы функции
+    :return:
+    '''
     def decorator(funk):
         def wrapper(*args, **kwargs):
             start_of_the_function = time()
+            input_parameters = (*args, *kwargs)
+
             try:
                 result = funk(*args, **kwargs)
-            except Exception as result:
-                pass
-                # input_parameters = (*args, **kwargs)
-            finally:
+                message = f"{funk.__name__}\nresut = {result}"
+
+            except Exception as e:
                 stop_of_the_function = time()
+                message = f"""{funk.__name__}\nerorr: {e}\ninput parameters: {input_parameters}
+start of the function = {start_of_the_function}\nstop of the function = {stop_of_the_function}\n\n"""
+
+            finally:
                 if filename:
                     with open(filename, "a") as file_log:
-                        file_log.write(
-                            f"""{filename}\n
-                            resut = {result}\n
-                            start_of_the_function = {start_of_the_function}\n
-                            stop_of_the_function = {stop_of_the_function}\n\n"""
-                        )
+                        file_log.write(message)
                 else:
-                    print(
-                        f"""{filename}\n
-                            resut = {result}\n
-                            start_of_the_function = {start_of_the_function}\n
-                            stop_of_the_function = {stop_of_the_function}\n\n"""
-                    )
+                    print(message)
+
         return wrapper
+
     return decorator
 
 
-@log(filename="mylog.txt")
+# @log()
+# def my_function():
+#     raise Exception("СООБЩЕНИЕ !!! !!!")
+#
+#
+# my_function()
+
+
+@log("her.txt")
 def my_function(x, y):
-    return x + y
+    aaaa = x + y
+    raise Exception("СООБЩЕНИЕ !!! !!!")
+
 
 my_function(1, 2)
